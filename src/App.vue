f0e2ff2c48ad877afd0c05353a6a166f05ab57ad
Philipp Wallrich <pwallrich@inovex.de>
2020-10-09 01:10:42 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+    Meetinator
+    </v-app-bar>
+    <v-main>
+      <Meetinator/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Meetinator from './components/Meetinator';
+
+export default {
+  name: 'App',
+
+  components: {
+    Meetinator,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/Meetinator.vue
@@ -0,0 +1,107 @@
+<template>
+  <div>
+    <v-simple-table>
+      <template v-slot:default> 
+        <thead>
+          <tr>
+            <th class="text-left">
+              Name
+            </th>
+            <th class="text-left">
+              Do 15. Okt
+            </th>
+            <th>
+              ich komme nicht
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr 
+            v-for="(person, index) in persons"
+            :key="person.name"
+          >
+            <td>
+              <v-text-field
+                label="Name"
+                :ref="index"
+                :value="person.name"
+              >
+                Kerstin
+              </v-text-field>
+            </td>
+            <td>
+              <v-checkbox
+                :value="0"
+                v-model="person.selectedIndex"
+              >
+              </v-checkbox>
+            </td>
+            <td>
+              <v-checkbox
+              :value="1"
+              v-model="person.selectedIndex"
+              
+              >
+              </v-checkbox>
+            </td>
+            <td>
+              <v-btn
+                @click="deleteRow(index)"
+              >
+                <v-icon>mdi-trash-can-outline</v-icon>
+              </v-btn>
+            </td>
+          </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
+    <v-btn
+        @click="addRow"
+      >
+        <v-icon
+          left
+          large
+          color="green"
+        >mdi-plus-circle</v-icon>
+        neuen Eintrag hinzufügen
+      </v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      persons: [
+        {
+          name: "Kerstin",
+          selectedIndex: [],
+        },
+        {
+          name: "Kerstin 2",
+          selectedIndex: [0],
+        },
+        {
+          name: "Kerstin 3",
+          selectedIndex: [1],
+        }
+      ]
+    }
+  },
+  methods: {
+    addRow: function() {
+      this.persons.push({
+        selectedIndex: []
+      })
+      console.log(this.$refs)
+      this.$nextTick(function() {
+        const index = this.persons.length - 1
+        this.$refs[index][0].focus()
+      })
+    },
+    deleteRow: function(index) {
+      this.persons.splice(index, 1)
+    }
+  }
+}
+</script>
