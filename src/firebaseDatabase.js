2604fed6dbba7e52c47dc4f627fd0a69913401b2
Philipp Wallrich <pwallrich@inovex.de>
2020-10-13 13:47:40 +0200

    Readds firebase database with .env variables after rewriting history

---
+++ src/firebaseDatabase.js
@@ -0,0 +1,23 @@
+import * as firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+    authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.VUE_APP_FIREBASE_APP_ID,
+    measurementId: process.env.VUE_APP_FIREBASE_MEASUREMENT_ID
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+const db = firebaseApp.firestore()
+
+const personsCollection = db.collection('persons')
+
+export {
+    db,
+    personsCollection
+}
